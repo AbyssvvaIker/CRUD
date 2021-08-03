191567237b578188c023ba6eafd3fb75572b6f51
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-03 14:49:25 +0200

    add: seed base if empty

---
+++ Warehouse.WebMvc/Models/SeedCategory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using Warehouse.WebMvc.Data;
+
+namespace Warehouse.WebMvc.Models
+{
+    public static class SeedCategory
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcCategoryContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcCategoryContext>>()))
+            {
+                if (context.Category.Any())
+                {
+                    return;
+                }
+
+                context.Category.AddRange(
+                    new Category
+                    {
+                        Name = "Food"
+                    });
+                    new Category
+                    {
+                        Name = "PC"
+                    };
+                    new Category
+                    {
+                        Name = "Car"
+                    };
+                context.SaveChanges();
+            }
+        }
+    }
+}
