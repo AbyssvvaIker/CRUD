8bda171dc7cce573054703553ed1ccdfc02d6dec
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-03 12:02:11 +0200

    Add Data folder and CategoryContext class to it

---
+++ Warehouse.WebMvc/Data/MvcCategoryContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Warehouse.WebMvc.Models;
+
+namespace Warehouse.WebMvc.Data
+{
+    public class MvcCategoryContext : DbContext
+    {
+        public MvcCategoryContext(DbContextOptions<MvcCategoryContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Movie { get; set; }
+    }
+}
