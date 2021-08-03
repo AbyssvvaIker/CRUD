e165dda8cad7e76c491eab38e87270a18833f0a4
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-03 13:52:47 +0200

    fix: new db model migration

--- Warehouse.WebMvc/Migrations/20210803115212_002_modelChange.Designer.cs
+++ Warehouse.WebMvc/Migrations/20210803115212_002_modelChange.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Warehouse.WebMvc.Data;
 
 namespace Warehouse.WebMvc.Migrations
 {
     [DbContext(typeof(MvcCategoryContext))]
-    partial class MvcCategoryContextModelSnapshot : ModelSnapshot
+    [Migration("20210803115212_002_modelChange")]
+    partial class _002_modelChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
