4a094feb4f5320a581660e65209ec614c0743e18
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-04 11:08:47 +0200

    fix: correct namespace

--- Warehouse.Core/Entities/Category.cs
+++ Warehouse.Core/Entities/Category.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace Warehouse.Core
+namespace Warehouse.Core.Entities
 {
     public class Category
     {
