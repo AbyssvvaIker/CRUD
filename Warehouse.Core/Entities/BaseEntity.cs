6fb32fec0ee2abfcc0e5d4d22ea1db45d50bf11a
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-04 15:04:36 +0200

    add: constructor for BaseEntity with ID generation

--- Warehouse.Core/Entities/BaseEntity.cs
+++ Warehouse.Core/Entities/BaseEntity.cs
@@ -7,5 +7,10 @@ namespace Warehouse.Core.Entities
     public class BaseEntity
     {
         public Guid Id { get; set; }
+
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
     }
 }
