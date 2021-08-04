ed7063386be37159a8440cce73f8ca8a4482d360
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-04 14:13:09 +0200

    add: category view model

---
+++ Warehouse.WebMvc/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.WebMvc.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public CategoryViewModel(Guid Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+
+    }
+
+}
