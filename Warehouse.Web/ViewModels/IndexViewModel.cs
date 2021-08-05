a294e8153db1509a4e7e69b314a288ae1d250963
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-05 13:18:08 +0200

    fix: repo fix

---
+++ Warehouse.Web/ViewModels/IndexViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Web.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IList<IndexItemViewModel> Categories { get; set; }
+    
+        public IndexViewModel()
+        {
+            Categories = new List<IndexItemViewModel>();
+        }
+    }
+
+    public class IndexItemViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
