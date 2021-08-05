30fd93b27e999a3545a1a19da8404df03f17259b
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-05 16:36:41 +0200

    add: product model

---
+++ Warehouse.Core/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warehouse.Core.Entities
+{
+    class Product : BaseEntity
+    {
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
