38e7b1f6119a556f230488ed0a472bec6ae7b7d7
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-06 09:16:17 +0200

    add: product index view model

---
+++ Warehouse.Web/ViewModels/ProductIndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Warehouse.Web.ViewModels
+{
+    public class ProductIndexViewModel
+    {
+        public IList<ProductIndexItemViewModel> Products { get; set; }
+
+        public ProductIndexViewModel()
+        {
+            Products = new List<ProductIndexItemViewModel>();
+        }
+    }
+
+    public class ProductIndexItemViewModel
+    {
+        public Guid Id { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
