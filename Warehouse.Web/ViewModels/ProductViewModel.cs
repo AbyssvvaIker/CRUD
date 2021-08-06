5102057f78a95ef1f4f1edf00a3747c745c9a96f
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-06 13:01:39 +0200

    fix: ProductViewModel.Category type change

--- Warehouse.Web/ViewModels/ProductViewModel.cs
+++ Warehouse.Web/ViewModels/ProductViewModel.cs
@@ -8,7 +8,7 @@ namespace Warehouse.Web.ViewModels
     public class ProductViewModel
     {
         public Guid Id { get; set; }
-        public string Category { get; set; }
+        public Guid Category { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
