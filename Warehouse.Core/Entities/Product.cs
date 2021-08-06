8b544335eb30be8d0cfa9c708ed5332f436c1848
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-06 12:12:11 +0200

    add: one-to-many relation for category and product

--- Warehouse.Core/Entities/Product.cs
+++ Warehouse.Core/Entities/Product.cs
@@ -6,9 +6,11 @@ namespace Warehouse.Core.Entities
 {
     public class Product : BaseEntity
     {
-        public string Category { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
