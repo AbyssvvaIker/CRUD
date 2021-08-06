270472edd09c6e397c3b13a520beba54d5958d42
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-06 12:06:07 +0200

    fix: remove doubled formatting for category

--- Warehouse.Infrastructure.DataAccess/EntityConfigurations/ProductEntityConfiguration.cs
+++ Warehouse.Infrastructure.DataAccess/EntityConfigurations/ProductEntityConfiguration.cs
@@ -14,9 +14,7 @@ namespace Warehouse.Infrastructure.DataAccess.EntityConfigurations
             builder.Property(p => p.Name)
                 .IsRequired()
                 .HasMaxLength(50);
-            builder.Property(p => p.Category)
-                .IsRequired()
-                .HasMaxLength(50);
+            //Category is formatted in CategoryEntityConfiguration
             builder.Property(p => p.Description)
                 .IsRequired()
                 .HasMaxLength(256);
