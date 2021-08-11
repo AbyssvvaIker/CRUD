2f0e9f33f94f062fc8b7e2110c938315f16c013e
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-11 16:10:03 +0200

    add: category logic interface

---
+++ Warehouse.Core/Interfaces/ICategoryLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Core.Entities;
+
+namespace Warehouse.Core.Interfaces
+{
+    interface ICategoryLogic : ILogic
+    {
+        Task<Result<IEnumerable<Category>>> GetAllActiveAsync();
+        Task<Result<Category>> GetByIdAsync(Guid id);
+
+        Task<Result<Category>> AddAsync(Category product);
+        Task<Result<Category>> UpdateAsync(Category product);
+        Task<Result> DeleteAsync(Category product);
+    }
+}
