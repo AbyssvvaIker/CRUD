57c3aa837e7ae66a00db0100698666065ba1e5f0
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-11 17:47:34 +0200

    fix: properly place DataAccessModule.Load() & delete DeafultModule (will not be useful)

--- Warehouse.Web/Infrastructure/Autofac/Modules/DataAccessModule.cs
+++ Warehouse.Web/Infrastructure/Autofac/Modules/DataAccessModule.cs
@@ -3,10 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Warehouse.Core.Interfaces;
+using Warehouse.Infrastructure.DataAccess.Repositories;
 
 namespace Warehouse.Web.Infrastructure.Autofac.Modules
 {
     public class DataAccessModule : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(Repository<>).Assembly)
+                .AsClosedTypesOf(typeof(IRepository<>))
+                .AsImplementedInterfaces();
+        }
     }
 }
