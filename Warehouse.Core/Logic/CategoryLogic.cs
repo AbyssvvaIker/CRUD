04bef3d92010eb04d32858cfbeb3c6513d479a7c
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-11 16:29:26 +0200

    add: CategoryLogic and ProductLogic

---
+++ Warehouse.Core/Logic/CategoryLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Core.Entities;
+using Warehouse.Core.Interfaces;
+
+namespace Warehouse.Core.Logic
+{
+    class CategoryLogic : ICategoryLogic
+    {
+        public Task<Result<Category>> AddAsync(Category product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> DeleteAsync(Category product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<IEnumerable<Category>>> GetAllActiveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Category>> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Category>> UpdateAsync(Category product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Warehouse.Core/Logic/ProductLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Core.Entities;
+using Warehouse.Core.Interfaces;
+
+namespace Warehouse.Core.Logic
+{
+    class ProductLogic : IProductLogic
+    {
+        public Task<Result<Product>> AddAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> DeleteAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<IEnumerable<Product>>> GetAllActiveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Product>> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Product>> UpdateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
