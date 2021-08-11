ae5f141b8fe404b827c7b32bb1b579f418133439
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-11 17:44:20 +0200

    add: CoreModule.Load()

--- Warehouse.Web/Infrastructure/Autofac/Modules/CoreModule.cs
+++ Warehouse.Web/Infrastructure/Autofac/Modules/CoreModule.cs
@@ -3,10 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Warehouse.Core.Interfaces;
 
 namespace Warehouse.Web.Infrastructure.Autofac.Modules
 {
     public class CoreModule : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterAssemblyTypes(typeof(ILogic).Assembly)
+                .Where(t => typeof(ILogic).IsAssignableFrom(t))
+                .AsImplementedInterfaces();
+        }
     }
 }
