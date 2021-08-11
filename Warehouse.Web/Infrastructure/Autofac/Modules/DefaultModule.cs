214477e3ad7795b0d94a623474f0945ff7341b5a
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-11 17:27:40 +0200

    add: default module with overridden Load() method

---
+++ Warehouse.Web/Infrastructure/Autofac/Modules/DefaultModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Warehouse.Core.Interfaces;
+using Warehouse.Infrastructure.DataAccess.Repositories;
+
+namespace Warehouse.Web.Infrastructure.Autofac.Modules
+{
+    public class DefaultModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(Repository<>).Assembly)
+                .AsClosedTypesOf(typeof(IRepository<>))
+                .AsImplementedInterfaces();
+        }
+    }
+}
