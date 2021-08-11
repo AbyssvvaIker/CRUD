95c4ba1bb7255ec9194037fa036d11c266e61fd6
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-11 14:35:18 +0200

    add: Product repository class and interface

---
+++ Warehouse.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Warehouse.Core.Entities;
+
+namespace Warehouse.Core.Interfaces
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+    }
+}
---
+++ Warehouse.Infrastructure.DataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Warehouse.Core.Entities;
+using Warehouse.Core.Interfaces;
+
+namespace Warehouse.Infrastructure.DataAccess.Repositories
+{
+    class ProductRepository : Repository<Product>, IProductRepository 
+    {
+        public ProductRepository(DataContext db) : base(db)
+        {
+
+        }
+    }
+}
