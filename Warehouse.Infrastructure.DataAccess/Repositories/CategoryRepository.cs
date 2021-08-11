4fe64541f53d7efc8d1c9ee000d05f6a5abe2f94
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-11 13:47:54 +0200

    add: repositories classes files

---
+++ Warehouse.Infrastructure.DataAccess/Repositories/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Warehouse.Core.Entities;
+
+namespace Warehouse.Infrastructure.DataAccess.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(DataContext db) : base(db)
+        {
+
+        }
+    }
+}
---
+++ Warehouse.Infrastructure.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Warehouse.Core.Entities;
+
+namespace Warehouse.Infrastructure.DataAccess.Repositories
+{
+    public abstract class Repository<T> : IRepository<T> where T : BaseEntity, new()
+    {
+    }
+}
