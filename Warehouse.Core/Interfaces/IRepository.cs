2947b57243b3b51caa2d4c630aca18a04093eefa
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-12 16:00:33 +0200

    add: IRepository generic restriction

--- Warehouse.Core/Interfaces/IRepository.cs
+++ Warehouse.Core/Interfaces/IRepository.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Warehouse.Core.Entities;
 
 namespace Warehouse.Core.Interfaces
 {
-    public interface IRepository<T> 
+    public interface IRepository<T> where T : BaseEntity
     {
         Task<IEnumerable<T>> GetAllActiveAsync();
         Task<T> GetByIdAsync(Guid id);
