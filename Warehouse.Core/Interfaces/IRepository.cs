0b0c6cf9fe1ff7cb81db06e2afe27006194aefbb
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-12 10:01:37 +0200

    add: IRepository methods

--- Warehouse.Core/Interfaces/IRepository.cs
+++ Warehouse.Core/Interfaces/IRepository.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Warehouse.Core.Interfaces
 {
     public interface IRepository<T> 
     {
+        Task<IEnumerable<T>> GetAllActiveAsync();
+        Task<T> GetIdAsync(Guid id);
+        Task<T> AddAsync(T entity);
+        void Delete(T entity);
+        Task SaveChangesAsync();
+
     }
 }
