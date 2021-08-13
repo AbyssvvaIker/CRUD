647ee74d0404a45484a2ef2ada0fbe3aa70c9dfd
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-13 10:42:04 +0200

    fix: result extension method

--- Warehouse.Web/ExtensionMethods/ResultExtensions.cs
+++ Warehouse.Web/ExtensionMethods/ResultExtensions.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Warehouse.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Warehouse.Web.ExtensionMethods
 {
-    public class ResultExtensions
+    public static class ResultExtensions
     {
-        public static void AddErrorToModelState(this Result result, ModelState modelState)
+        public static void AddErrorToModelState(this Result result, ModelStateDictionary modelStateDictionary)
         {
-
+            foreach(var item in result.Errors)
+            {
+                modelStateDictionary.AddModelError(item.PropertyName, item.Message);
+            }
         }
     }
 }
