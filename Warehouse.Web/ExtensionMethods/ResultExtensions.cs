22924f01192d94fce41f7125f9d0f1ded3749fad
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-13 10:04:24 +0200

    add: extension method for Result.cs

---
+++ Warehouse.Web/ExtensionMethods/ResultExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Warehouse.Core;
+
+namespace Warehouse.Web.ExtensionMethods
+{
+    public class ResultExtensions
+    {
+        public static void AddErrorToModelState(this Result result, ModelState modelState)
+        {
+
+        }
+    }
+}
