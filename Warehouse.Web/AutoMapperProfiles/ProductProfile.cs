fd4a812eacea0f6930eb847e2a465d031be9782f
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-13 14:08:32 +0200

    add: new mappings for product

--- Warehouse.Web/AutoMapperProfiles/ProductProfile.cs
+++ Warehouse.Web/AutoMapperProfiles/ProductProfile.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Warehouse.Core.Entities;
+using Warehouse.Web.ViewModels.Product;
 
 namespace Warehouse.Web.AutoMapperProfiles
 {
-    public class ProductProfile
+    public class ProductProfile : Profile
     {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductViewModel>();
+            CreateMap<ProductViewModel, Product>();
+            CreateMap<Product, IndexItemViewModel>();
+        }
     }
 }
