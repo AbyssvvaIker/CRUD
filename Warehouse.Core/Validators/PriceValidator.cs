a599f841d77f970c524be560470b5077532634e0
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-16 16:30:25 +0200

    add: PriceValidator

---
+++ Warehouse.Core/Validators/PriceValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warehouse.Core.Validators
+{
+    public class PriceValidator<T, Tproperty> : PropertyValidator<T, Tproperty>
+    {
+        public override string Name => "PriceValidator";
+
+        public override bool IsValid(ValidationContext<T> context, Tproperty value)
+        {
+            var price = value is decimal ? Convert.ToDecimal(value) : 0;
+            return PriceValidate(price);
+        }
+
+        private bool PriceValidate(decimal price)
+        {
+            if(price <= decimal.Zero)
+            {
+                return false;
+            }
+            price *= 100;
+            if(price % 1 != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        protected override string GetDefaultMessageTemplate(string errorCode)
+        {
+            return "{PropertyName} has invalid format";
+        }
+
+    }
+}
