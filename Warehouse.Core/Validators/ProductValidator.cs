6b4a45125b431e09895560120cde77ca0b91f2cd
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-16 16:33:52 +0200

    fix: change validator for Price

--- Warehouse.Core/Validators/ProductValidator.cs
+++ Warehouse.Core/Validators/ProductValidator.cs
@@ -17,10 +17,12 @@ namespace Warehouse.Core.Validators
             RuleFor(x => x.Description).Length(1, 512)
                 .NotEmpty()
                 .WithMessage("{PropertyName}, it has to be from {MinLength} to {MaxLength} characters long");
-            RuleFor(x => x.Price).GreaterThan((decimal)0)
-                .ScalePrecision(2,6)
-                .NotEmpty()
-                .WithMessage("{PropertyName}, it has to be greater than {ComparisionValue}");
+            //RuleFor(x => x.Price).GreaterThan((decimal)0)
+            //    .ScalePrecision(2,6)
+            //    .NotEmpty()
+            //    .WithMessage("{PropertyName}, it has to be greater than {ComparisionValue}");
+            RuleFor(x => x.Price)
+                .SetValidator(new PriceValidator<Product, decimal>());
             RuleFor(x => x.Category).NotEmpty();
         }
     }
