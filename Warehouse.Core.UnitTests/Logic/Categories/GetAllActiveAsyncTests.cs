db0ebd4d39ab51dcb86efec4a12201ecb2ba2eac
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-17 15:15:40 +0200

    add: GetAllActive tests

--- Warehouse.Core.UnitTests/Logic/Categories/GetAllActiveAsyncTests.cs
+++ Warehouse.Core.UnitTests/Logic/Categories/GetAllActiveAsyncTests.cs
@@ -1,10 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using Moq;
+using FluentAssertions;
+using FizzWare.NBuilder;
+using Warehouse.Core.Interfaces.Repositories;
+using Warehouse.Core.Entities;
+using Warehouse.Core.Interfaces;
+using FluentValidation;
+using Warehouse.Core.Logic;
+using System.Threading.Tasks;
+using Warehouse.Core.UnitTests.Extensions;
 
 namespace Warehouse.Core.UnitTests.Logic.Categories
 {
-    class GetAllActiveAsyncTests
+    public class GetAllActiveAsyncTests
     {
+        [Fact]
+        public async Task ShouldReturnListOfCategories()
+        {
+            var listActive = Builder<Category>
+                .CreateListOfSize(3)
+                .All()
+                .With(x => x.Id = Guid.NewGuid())
+                .With(x => x.Name = "testName")
+                .With(x => x.IsActive = true)
+                .Build();
+
+            var mockCategoryRepository = new Mock<ICategoryRepository>();
+            mockCategoryRepository.Setup(x => x.GetAllActiveAsync()).ReturnsAsync(listActive);
+            var mockProductRepository = new Mock<IProductRepository>();
+            var mockValidator = new Mock<IValidator<Category>>();
+
+            var categoryLogic = new CategoryLogic(mockCategoryRepository.Object, mockProductRepository.Object, mockValidator.Object);
+
+            var result =await categoryLogic.GetAllActiveAsync();
+
+            result.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+            result.Value.Should().BeSameAs(listActive);
+            
+        }
+        [Fact]
+        public async Task ShouldReturnEmptyList()
+        {
+
+        }
     }
 }
