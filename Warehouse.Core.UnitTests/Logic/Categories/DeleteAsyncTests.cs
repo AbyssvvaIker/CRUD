13f91923803c4cc4ff67625a9890b4376a71aca5
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-17 14:40:58 +0200

    add: deleteAsync tests

--- Warehouse.Core.UnitTests/Logic/Categories/DeleteAsyncTests.cs
+++ Warehouse.Core.UnitTests/Logic/Categories/DeleteAsyncTests.cs
@@ -1,10 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Xunit;
+using Moq;
+using FluentAssertions;
+using FizzWare.NBuilder;
+using Warehouse.Core.Interfaces.Repositories;
+using Warehouse.Core.Entities;
+using Warehouse.Core.Interfaces;
+using FluentValidation;
+using Warehouse.Core.Logic;
+using System.Threading.Tasks;
+using Warehouse.Core.UnitTests.Extensions;
 namespace Warehouse.Core.UnitTests.Logic.Categories
 {
-    class DeleteAsyncTests
+    public class DeleteAsyncTests
     {
+        [Fact]
+        public async Task ShouldThrowArgumentNullException()
+        {
+            var mockCategoryRepository = new Mock<ICategoryRepository>();
+            var mockProductRepository = new Mock<IProductRepository>();
+            var mockValidator = new Mock<IValidator<Category>>();
+
+            var categoryLogic = new CategoryLogic(mockCategoryRepository.Object, mockProductRepository.Object, mockValidator.Object);
+
+            Func<Task> act = async () => await categoryLogic.DeleteAsync((Category)null);
+
+            await act.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task ShouldReturnResultOk()
+        {
+            var category = Builder<Category>
+                 .CreateNew()
+                 .With(x => x.Id = Guid.NewGuid())
+                 .With(x => x.Name = "testName")
+                 .Build();
+
+            var mockCategoryRepository = new Mock<ICategoryRepository>();
+            var mockProductRepository = new Mock<IProductRepository>();
+            var mockValidator = new Mock<IValidator<Category>>();
+
+            var categoryLogic = new CategoryLogic(mockCategoryRepository.Object, mockProductRepository.Object, mockValidator.Object);
+
+            var result =await categoryLogic.DeleteAsync(category);
+
+            result.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+        }
     }
 }
