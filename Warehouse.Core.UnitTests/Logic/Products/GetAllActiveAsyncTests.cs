6e4d34cfcc7c5f0b063a1611769e748460f45688
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-17 17:17:34 +0200

    add: getAllActiveAsync tests for products

---
+++ Warehouse.Core.UnitTests/Logic/Products/GetAllActiveAsyncTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using FluentAssertions;
+using FizzWare.NBuilder;
+using Warehouse.Core.Interfaces.Repositories;
+using Warehouse.Core.Entities;
+using Warehouse.Core.Interfaces;
+using FluentValidation;
+using Warehouse.Core.Logic;
+using System.Threading.Tasks;
+using Warehouse.Core.UnitTests.Extensions;
+
+namespace Warehouse.Core.UnitTests.Logic.Products
+{
+    public class GetAllActiveAsyncTests
+    {
+        [Fact]
+        public async Task ShouldReturnListOfProducts()
+        {
+            var listActive = Builder<Product>
+                .CreateListOfSize(3)
+                .All()
+                .With(x => x.Id = Guid.NewGuid())
+                .With(x => x.IsActive = true)
+                .Build();
+
+            var mockProductRepository = new Mock<IProductRepository>();
+            mockProductRepository.Setup(x => x.GetAllActiveAsync()).ReturnsAsync(listActive);
+            var mockValidator = new Mock<IValidator<Product>>();
+
+            var productLogic = new ProductLogic(mockProductRepository.Object, mockValidator.Object);
+
+            var result = await productLogic.GetAllActiveAsync();
+
+            result.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+            result.Value.Should().BeSameAs(listActive);
+
+        }
+        [Fact]
+        public async Task ShouldReturnEmptyList()
+        {
+            var listActive = new List<Product>();
+
+            var mockProductRepository = new Mock<IProductRepository>();
+            mockProductRepository.Setup(x => x.GetAllActiveAsync()).ReturnsAsync(listActive);
+            var mockValidator = new Mock<IValidator<Product>>();
+
+            var productLogic = new ProductLogic(mockProductRepository.Object, mockValidator.Object);
+
+            var result = await productLogic.GetAllActiveAsync();
+
+            result.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+            result.Value.Should().BeSameAs(listActive);
+        }
+    }
+}
