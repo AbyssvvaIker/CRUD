a17926d83f9736aa76967e1d68d0037b3eae6554
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-17 17:27:01 +0200

    add: UpdateAsync tests for Products

---
+++ Warehouse.Core.UnitTests/Logic/Products/UpdateAsyncTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using FluentAssertions;
+using FizzWare.NBuilder;
+using Warehouse.Core.Interfaces.Repositories;
+using Warehouse.Core.Entities;
+using Warehouse.Core.Interfaces;
+using FluentValidation;
+using Warehouse.Core.Logic;
+using System.Threading.Tasks;
+using Warehouse.Core.UnitTests.Extensions;
+
+namespace Warehouse.Core.UnitTests.Logic.Products
+{
+    public class UpdateAsyncTests
+    {
+        [Fact]
+        public async Task ShouldThrowArgumentNullException()
+        {
+            var mockProductRepository = new Mock<IProductRepository>();
+            var mockValidator = new Mock<IValidator<Product>>();
+
+            var productLogic = new ProductLogic(mockProductRepository.Object, mockValidator.Object);
+
+            Func<Task> act = async () => await productLogic.UpdateAsync(null);
+
+            await act.Should().ThrowAsync<ArgumentNullException>();
+            
+        }
+
+        [Fact]
+        public async Task ShouldReturnResultFailure()
+        {
+            var product = Builder<Product>
+                .CreateNew()
+                .Build();
+
+            var mockProductRepository = new Mock<IProductRepository>();
+            var mockValidator = new Mock<IValidator<Product>>();
+            mockValidator.SetValidationFailure("test", "test error message");
+
+            var productLogic = new ProductLogic(mockProductRepository.Object, mockValidator.Object);
+
+            var result = await productLogic.UpdateAsync(product);
+
+            result.Should().NotBeNull();
+            result.Success.Should().BeFalse();
+            result.Errors.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public async Task ShouldReturnResultOk()
+        {
+            var product = Builder<Product>
+                .CreateNew()
+                .With(x => x.Id = Guid.NewGuid())
+                .Build();
+
+            var mockProductRepository = new Mock<IProductRepository>();
+            var mockValidator = new Mock<IValidator<Product>>();
+            var productLogic = new ProductLogic(mockProductRepository.Object, mockValidator.Object);
+            mockValidator.SetValidationSuccess();
+         
+            var result = await productLogic.UpdateAsync(product);
+            
+            result.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+            result.Value.Should().BeSameAs(product);
+        }
+    }
+}
