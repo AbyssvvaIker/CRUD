76f39b767699f19f61efa54d253d002c5d6df851
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-17 17:10:54 +0200

    add: DeleteAsync tests for Products

---
+++ Warehouse.Core.UnitTests/Logic/Products/DeleteAsyncTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using FluentAssertions;
+using FizzWare.NBuilder;
+using Warehouse.Core.Interfaces.Repositories;
+using Warehouse.Core.Entities;
+using Warehouse.Core.Interfaces;
+using FluentValidation;
+using Warehouse.Core.Logic;
+using System.Threading.Tasks;
+using Warehouse.Core.UnitTests.Extensions;
+
+namespace Warehouse.Core.UnitTests.Logic.Products
+{
+    public class DeleteAsyncTests
+    {
+        [Fact]
+        public async Task ShouldThrowArgumentNullException()
+        {
+            var mockProductRepository = new Mock<IProductRepository>();
+            var mockValidator = new Mock<IValidator<Product>>();
+
+            var productLogic = new ProductLogic(mockProductRepository.Object, mockValidator.Object);
+
+            Func<Task> act = async () => await productLogic.DeleteAsync(null);
+
+            await act.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task ShouldReturnResultOk()
+        {
+            var product = Builder<Product>
+                 .CreateNew()
+                 .With(x => x.Id = Guid.NewGuid())
+                 .Build();
+
+            var mockProductRepository = new Mock<IProductRepository>();
+            var mockValidator = new Mock<IValidator<Product>>();
+
+            var productLogic = new ProductLogic(mockProductRepository.Object, mockValidator.Object);
+
+            var result = await productLogic.DeleteAsync(product);
+
+            result.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+        }
+    }
+}
