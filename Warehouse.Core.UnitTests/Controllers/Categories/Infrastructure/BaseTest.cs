329eadde6726dfcdc3dba93b75e2b2e29fae5e34
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-19 12:17:14 +0200

    add: BaseTest for Category and Products Controllers

---
+++ Warehouse.Core.UnitTests/Controllers/Categories/Infrastructure/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Warehouse.Core.Interfaces;
+using Warehouse.Web.Controllers;
+
+namespace Warehouse.Core.UnitTests.Controllers.Categories.Infrastructure
+{
+    public class BaseTest
+    {
+        protected Mock<ICategoryLogic> MockCategoryLogic { get; set; }
+        protected Mock<IMapper> MockMapper { get; set; }
+
+        protected virtual CategoryController Create()
+        {
+            MockCategoryLogic = new Mock<ICategoryLogic>();
+            MockMapper = new Mock<IMapper>();
+
+            return new CategoryController(MockCategoryLogic.Object, MockMapper.Object);
+        }
+    }
+}
