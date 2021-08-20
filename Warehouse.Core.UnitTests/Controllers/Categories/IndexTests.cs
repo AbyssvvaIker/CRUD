96e15c6c7d654a7001c20e5edc585984e7d8089d
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-20 13:15:04 +0200

    add: Index test

--- Warehouse.Core.UnitTests/Controllers/Categories/IndexTests.cs
+++ Warehouse.Core.UnitTests/Controllers/Categories/IndexTests.cs
@@ -1,10 +1,58 @@
-﻿using System;
+﻿using FizzWare.NBuilder;
+using FluentAssertions.AspNetCore.Mvc;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Warehouse.Core.Entities;
+using Warehouse.Core.UnitTests.Controllers.Categories.Infrastructure;
+using Warehouse.Web.Controllers;
+using Warehouse.Web.ViewModels.Category;
+using Xunit;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Warehouse.Core.UnitTests.Controllers.Categories
 {
-    class IndexTests
+    public class IndexTests : BaseTest
     {
+        protected IList<IndexItemViewModel> Categories { get; set; }
+        protected IList<IndexItemViewModel> ViewModel { get; set; }
+        protected Result<IEnumerable<Category>> CategoriesResult { get; set; }
+
+        protected override CategoryController Create()
+        {
+            var controller = base.Create();
+            Categories = Builder<IndexItemViewModel>
+                .CreateListOfSize(5)
+                .Build();
+
+            ViewModel = Builder<IndexItemViewModel>
+                .CreateListOfSize(5)
+                .Build();
+
+            CategoriesResult = Builder<Result<IEnumerable<Category>>>
+            .CreateNew()
+            .Build();
+            //MockCategoryLogic.Setup( x =>x.GetAllActiveAsync()).ReturnsAsync(() => CategoriesResult);
+            MockCategoryLogic.Setup(x => x.GetAllActiveAsync()).ReturnsAsync(CategoriesResult);
+            MockMapper.Setup(x => x.Map<IList<IndexItemViewModel>>(CategoriesResult.Value)).Returns(Categories);
+
+            return controller;
+        }
+
+        [Fact]
+        public async Task Should_Return_View_With_ListOf_IndexViewModel()
+        {
+            var controller = Create();
+
+            var result =await controller.Index();
+
+            result.Should()
+                .BeViewResult()
+                .WithDefaultViewName()
+                .Model
+                .Should()
+                .BeEquivalentTo(ViewModel);
+        }
     }
 }
