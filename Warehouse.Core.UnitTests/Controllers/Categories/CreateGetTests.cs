3869898e718cd13e4ab12281c95510e656cd8a24
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-20 14:13:11 +0200

    add: CreateGetTests with tests

---
+++ Warehouse.Core.UnitTests/Controllers/Categories/CreateGetTests.cs
@@ -0,0 +1,45 @@
+﻿using FizzWare.NBuilder;
+using FluentAssertions.AspNetCore.Mvc;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Core.Entities;
+using Warehouse.Core.UnitTests.Controllers.Categories.Infrastructure;
+using Warehouse.Web.Controllers;
+using Warehouse.Web.ViewModels.Category;
+using Xunit;
+
+namespace Warehouse.Core.UnitTests.Controllers.Categories
+{
+    public class CreateGetTests : BaseTest
+    {
+        protected CategoryViewModel ViewModel { get; set; }
+
+        protected override CategoryController Create()
+        {
+            var controller = base.Create();;
+
+            return controller;
+        }
+
+        [Fact]
+        public async Task Should_Return_View_With_Empty_ViewModel()
+        {
+            //arrange
+            var controller = Create();
+            //act
+            var result = controller.Create();
+            //assert
+            result.Should()
+                .BeViewResult()
+                .WithDefaultViewName()
+                .Model
+                .Should()
+                .BeOfType(typeof(CategoryViewModel));
+
+        }
+    }
+}
