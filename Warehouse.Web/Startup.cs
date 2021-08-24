31cd492517eab3d116783f8115cd8cd2c4ea391b
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-24 14:51:37 +0200

    add: swagger service configuration

--- Warehouse.Web/Startup.cs
+++ Warehouse.Web/Startup.cs
@@ -15,6 +15,10 @@ using FluentValidation.AspNetCore;
 using FluentValidation;
 using Warehouse.Core.Entities;
 using Warehouse.Core.Validators;
+using Microsoft.OpenApi.Models;
+using Autofac;
+using System.Reflection;
+using System.IO;
 
 namespace Warehouse.Web
 {
@@ -33,8 +37,21 @@ namespace Warehouse.Web
             services.AddControllersWithViews();
             services.AddDbContext<DataContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("name",
+                    new OpenApiInfo
+                    {
+                        Title = "Warehouse.Api",
+                        Version = "v1"
+                    });
+                x.CustomSchemaIds((type) => type.FullName);
 
+                var xmlFile = $"{Assembly.GetEntryAssembly()?.GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                x.IncludeXmlComments(xmlPath);
+                x.DescribeAllParametersInCamelCase();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
