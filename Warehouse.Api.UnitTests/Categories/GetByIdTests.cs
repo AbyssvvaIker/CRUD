03182696840bc428f6e03b3fc9c0872d50f83a2b
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-25 14:52:48 +0200

    add: create methods

--- Warehouse.Api.UnitTests/Categories/GetByIdTests.cs
+++ Warehouse.Api.UnitTests/Categories/GetByIdTests.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using FizzWare.NBuilder;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Warehouse.Api.Controllers;
+using Warehouse.Api.DTOs.Category;
+using Warehouse.Api.UnitTests.Categories.Infrastructure;
+using Warehouse.Core;
+using Warehouse.Core.Entities;
 
 namespace Warehouse.Api.UnitTests.Categories
 {
-    class GetByIdTests
+    public class GetByIdTests : BaseTest
     {
+        protected Result<Category> ResultCategory { get; set; }
+        protected Result<CategoryDto> ResultCategoryDto { get; set; }
+        protected CategoryDto Dto { get; set; }
+        protected override CategoryController Create()
+        {
+            var controller = base.Create();
+            ResultCategory = Builder<Result<Category>>.CreateNew()
+                .Build();
+
+            Dto = Builder<CategoryDto>.CreateNew()
+                .Build();
+
+            ResultCategoryDto = Result.Ok(Dto);
+
+            MockCategoryLogic.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(ResultCategory);
+            MockMapper.Setup(x => x.Map<CategoryDto>(It.IsAny<CategoryDto>())).Returns(Dto);
+
+            return controller;
+        }
     }
 }
--- Warehouse.Api.UnitTests/Categories/Infrastructure/BaseTest.cs
+++ Warehouse.Api.UnitTests/Categories/Infrastructure/BaseTest.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using AutoMapper;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Warehouse.Api.Controllers;
+using Warehouse.Core.Interfaces;
 
 namespace Warehouse.Api.UnitTests.Categories.Infrastructure
 {
     public class BaseTest
     {
+        protected Mock<ICategoryLogic> MockCategoryLogic { get; set; }
+        protected Mock<IMapper> MockMapper { get; set; }
+
+        protected virtual CategoryController Create()
+        {
+            MockCategoryLogic = new Mock<ICategoryLogic>();
+            MockMapper = new Mock<IMapper>();
+
+            return new CategoryController(MockCategoryLogic.Object, MockMapper.Object);
+        }
     }
 }
