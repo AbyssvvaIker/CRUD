1ccdf5ef6d32e2200c7c24edf288e0f75e6556e9
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-26 11:02:01 +0200

    add: Products api test

---
+++ Warehouse.Api.UnitTests/Products/DeleteTests.cs
@@ -0,0 +1,85 @@
+﻿using FizzWare.NBuilder;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Api.Controllers;
+using Warehouse.Api.DTOs.Category;
+using Warehouse.Api.UnitTests.Products.Infrastructure;
+using Warehouse.Core;
+using Warehouse.Core.Entities;
+using Xunit;
+
+namespace Warehouse.Api.UnitTests.Products
+{
+    public class DeleteTests : BaseTest
+    {
+        protected Category Category { get; set; }
+        protected Result<Category> GetResult { get; set; }
+        protected Result<Category> DeleteResult { get; set; }
+        protected Result<CategoryDto> DtoResult { get; set; }
+        protected CategoryDto Dto { get; set; }
+        protected override CategoryController Create()
+        {
+            var controller = base.Create();
+
+            Category = Builder<Category>.CreateNew()
+                .Build();
+
+            Dto = Builder<CategoryDto>.CreateNew()
+                .Build();
+
+            GetResult = Result.Ok(Category);
+            DeleteResult = Result.Ok(Category);
+            DtoResult = Result.Ok(Dto);
+
+            MockProductLogic.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(GetResult);
+            MockProductLogic.Setup(x => x.DeleteAsync(It.IsAny<Category>())).ReturnsAsync(DeleteResult);
+
+            return controller;
+        }
+        [Fact]
+        public async Task Should_Be_NotFound_When_GetResultIs_Failure()
+        {
+            var controller = Create();
+            var property = "property";
+            var message = "message";
+            GetResult = Result.Failure<Category>(property, message);
+            //act
+            var result = await controller.Delete(Category.Id);
+            //assert
+            result.Should()
+                .BeNotFound<Result<Category>>(property, message, string.Empty);
+            MockProductLogic.Verify(x =>
+            x.GetByIdAsync(Category.Id),
+            Times.Once);
+            MockProductLogic.Verify(x =>
+            x.DeleteAsync(It.IsAny<Category>()),
+            Times.Never);
+        }
+
+        [Fact]
+        public async Task Should_Be_BadRequest_When_DeleteResultIs_Failure()
+        {
+
+            var controller = Create();
+            var property = "property";
+            var message = "message";
+            DeleteResult = Result.Failure<Category>(property, message);
+            //act
+            var result = await controller.Delete(Category.Id);
+            //assert
+            result.Should()
+                .BeBadRequest<Result<Category>>(property, message, string.Empty);
+
+            MockProductLogic.Verify(x =>
+            x.GetByIdAsync(Category.Id),
+            Times.Once);
+            MockProductLogic.Verify(x =>
+            x.DeleteAsync(Category),
+            Times.Once);
+        }
+
+    }
+}
---
+++ Warehouse.Api.UnitTests/Products/GetAllActiveTests.cs
@@ -0,0 +1,82 @@
+﻿using FizzWare.NBuilder;
+//using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Api.Controllers;
+using Warehouse.Api.DTOs.Category;
+using Warehouse.Api.UnitTests.Products.Infrastructure;
+using Warehouse.Core;
+using Warehouse.Core.Entities;
+using Xunit;
+
+namespace Warehouse.Api.UnitTests.Products
+{
+    public class GetAllActiveTests : BaseTest
+    {
+        protected IEnumerable<Category> Categories { get; set; }
+        protected Result<IEnumerable<Category>> CategoriesResult { get; set; }
+        protected Result<IEnumerable<CategoryDto>> DtoResult { get; set; }
+        protected IEnumerable<CategoryDto> Dto { get; set; }
+        protected override CategoryController Create()
+        {
+            var controller = base.Create();
+
+            Categories = Builder<Category>.CreateListOfSize(5)
+                .Build();
+
+            Dto = Builder<CategoryDto>.CreateListOfSize(5)
+                .Build();
+
+            CategoriesResult = Result.Ok(Categories);
+            DtoResult = Result.Ok(Dto);
+
+            MockProductLogic.Setup(x => x.GetAllActiveAsync()).ReturnsAsync(CategoriesResult);
+            MockMapper.Setup(x => x.Map<IEnumerable<CategoryDto>>(It.IsAny<IEnumerable<Category>>()))
+                .Returns(Dto);
+
+            return controller;
+        }
+        [Fact]
+        public async Task Shoukd_Be_BadRequest_When_ResultIs_Failure()
+        {
+            //arrange
+            var controller = Create();
+            var property = "property";
+            var message = "message";
+            CategoriesResult = Result.Failure<IEnumerable<Category>>(property, message);
+            //act
+            var result =await controller.GetAllActive();
+            //assert
+            result.Should()
+                .BeBadRequest<Result<IEnumerable<CategoryDto>>>(property, message);
+
+            MockProductLogic.Verify(x =>
+            x.GetAllActiveAsync(),
+            Times.Once);
+            MockMapper.Verify(x =>
+            x.Map<IEnumerable<CategoryDto>>(It.IsAny<IEnumerable<Category>>()),
+            Times.Never);
+        }
+        [Fact]
+        public async Task Shoukd_Be_Ok_When_ResultIs_Ok()
+        {
+            //arrange
+            var controller = Create();
+            //act
+            var result = await controller.GetAllActive();
+            //assert
+            result.Should()
+                .BeOk(DtoResult);
+
+            MockProductLogic.Verify(x =>
+            x.GetAllActiveAsync(),
+            Times.Once);
+            MockMapper.Verify(x =>
+            x.Map<IEnumerable<CategoryDto>>(Categories),
+            Times.Once);
+        }
+    }
+}
