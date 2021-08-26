980a83b560a35aa26ae2adfe9a2c8cdd1d83d51a
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-26 09:11:20 +0200

    add: tests to getById()

--- Warehouse.Api.UnitTests/Categories/GetByIdTests.cs
+++ Warehouse.Api.UnitTests/Categories/GetByIdTests.cs
@@ -1,36 +1,75 @@
 ﻿using FizzWare.NBuilder;
+//using FluentAssertions.AspNetCore.Mvc;
+//using FluentAssertions;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Warehouse.Api.Controllers;
 using Warehouse.Api.DTOs.Category;
 using Warehouse.Api.UnitTests.Categories.Infrastructure;
 using Warehouse.Core;
 using Warehouse.Core.Entities;
+using Xunit;
 
 namespace Warehouse.Api.UnitTests.Categories
 {
     public class GetByIdTests : BaseTest
     {
+        protected Category Category { get; set; }
         protected Result<Category> ResultCategory { get; set; }
         protected Result<CategoryDto> ResultCategoryDto { get; set; }
         protected CategoryDto Dto { get; set; }
         protected override CategoryController Create()
         {
             var controller = base.Create();
-            ResultCategory = Builder<Result<Category>>.CreateNew()
+
+            Category = Builder<Category>.CreateNew()
                 .Build();
 
             Dto = Builder<CategoryDto>.CreateNew()
                 .Build();
 
+            ResultCategory = Result.Ok(Category);
             ResultCategoryDto = Result.Ok(Dto);
 
             MockCategoryLogic.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(ResultCategory);
-            MockMapper.Setup(x => x.Map<CategoryDto>(It.IsAny<CategoryDto>())).Returns(Dto);
+            MockMapper.Setup(x => x.Map<CategoryDto>(It.IsAny<Category>())).Returns(Dto);
 
             return controller;
+        }
+
+        [Fact]
+        public async Task Should_Be_NotFound_When_ResultIs_Failure()
+        {
+            //arrange
+            var controller = Create();
+            var property = "property";
+            var message = "message";
+            ResultCategory = Result.Failure<Category>(property, message);
+
+            MockCategoryLogic.Setup(x => x.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(ResultCategory);
+            //act
+            var result =await controller.GetById(Category.Id);
+            //assert
+            result.Should()
+                .BeNotFound<Result<Category>>(message);
+
+
+
+        }
+
+        [Fact]
+        public async Task Should_Return_ResultDto_When_ResultIs_Ok()
+        {
+            //arrange
+            var controller = Create();
+            //act
+            var result =await controller.GetById(Category.Id);
+            //assert
+            result.Should()
+                .BeOk(ResultCategoryDto);
         }
     }
 }
---
+++ Warehouse.Api.UnitTests/Extensions/ActionResultExtensions.cs
@@ -0,0 +1,216 @@
+﻿using System.Linq;
+using FluentAssertions.AspNetCore.Mvc;
+using FluentAssertions;
+using FluentAssertions.Common;
+using FluentAssertions.Execution;
+using FluentAssertions.Primitives;
+using Warehouse.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Warehouse.Core
+{
+    public static class ActionResultExtensions
+    {
+        public static ActionResultAssertions Should(this IActionResult actionResult)
+        {
+            return new ActionResultAssertions(actionResult);
+        }
+    }
+
+    public class ActionResultAssertions : ReferenceTypeAssertions<IActionResult, ActionResultAssertions>
+    {
+        public ActionResultAssertions(IActionResult actionResult) : base(actionResult)
+        {
+        }
+
+        protected override string Identifier => "actionResult";
+
+        public ActionResultAssertions BeOk<T>(T value, string because = "", params object[] becauseArgs)
+        {
+            var subject = Subject.As<ObjectResult>();
+            var subjectValue = subject.Value.As<Result<T>>();
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subject != null)
+                .FailWith("The ObjectResult can't be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subject.StatusCode == 200)
+                .FailWith("The StatusCode should be 200.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Success == true)
+                .FailWith("The Result.Success should be true.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Value != null)
+                .FailWith("The Result.Value can't be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Value.Equals(value))
+                .FailWith("The Value should be the same as expected.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Errors != null)
+                .FailWith("The Errors can't be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Errors.Any() == false)
+                .FailWith("The Errors shouldn't have errors.");
+
+            return this;
+        }
+
+        public ActionResultAssertions BeCreatedAtAction<T>(T value, string because = "", params object[] becauseArgs)
+        {
+            var subject = Subject.As<ObjectResult>();
+            var subjectValue = subject.Value.As<Result<T>>();
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subject != null)
+                .FailWith("The ObjectResult can't be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subject.StatusCode == 201)
+                .FailWith("The StatusCode should be 201.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Success == true)
+                .FailWith("The Result.Success should be true.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Value != null)
+                .FailWith("The Result.Value can't be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Value.Equals(value))
+                .FailWith("The Value should be the same as expected.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Errors != null)
+                .FailWith("The Errors can't be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Errors.Any() == false)
+                .FailWith("The Errors shouldn't have errors.");
+
+            return this;
+        }
+
+        public ActionResultAssertions BeNotFound<T>(string message, string because = "", params object[] becauseArgs)
+        {
+            var subject = Subject.As<ObjectResult>();
+            var subjectValue = subject.Value.As<Result<T>>();
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subject != null)
+                .FailWith("The ObjectResult can't be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subject.StatusCode == 404)
+                .FailWith("The StatusCode should be 404.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Success == false)
+                .FailWith("The Result.Success should be false.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Value == null)
+                .FailWith("The Result.Value should be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Errors != null)
+                .FailWith("The Errors can't be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Errors.Any())
+                .FailWith("The Errors should have errors.");
+
+            var error = subjectValue.Errors.FirstOrDefault();
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(error.Message == message)
+                .FailWith($"The Message should be '{message}'.");
+
+            return this;
+        }
+
+        public ActionResultAssertions BeBadRequest<T>(string property, string message, string because = "", params object[] becauseArgs)
+        {
+            var subject = Subject.As<ObjectResult>();
+            var subjectValue = subject.Value.As<Result<T>>();
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subject != null)
+                .FailWith("The ObjectResult can't be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subject.StatusCode == 400)
+                .FailWith("The StatusCode should be 400.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Success == false)
+                .FailWith("The Result.Success should be false.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Value == null)
+                .FailWith("The Result.Value should be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Errors != null)
+                .FailWith("The Errors can't be null.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(subjectValue.Errors.Any())
+                .FailWith("The Errors should have errors.");
+
+            var propertyError = subjectValue.Errors.FirstOrDefault(e => e.PropertyName == property);
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(propertyError != null)
+                .FailWith($"The Errors should contains error for property '{property}'.");
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(propertyError.Message == message)
+                .FailWith($"The Message for property '{property}' should be '{message}'.");
+
+            return this;
+        }
+
+        public ActionResultAssertions BeBadRequest<T>(string message, string because = "", params object[] becauseArgs)
+        {
+            BeBadRequest<T>(string.Empty, message, because, becauseArgs);
+
+            return this;
+        }
+    }
+}
