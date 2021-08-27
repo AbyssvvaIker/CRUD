d9b34f51e7a6665f2ceb524b3fc89156ecd39836
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-27 14:05:17 +0200

    add: stopwatch action filter

---
+++ Warehouse.Common/Filters/StopwatchFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace Warehouse.Common.Filters
+{
+    public class StopwatchFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            await next();
+            stopwatch.Stop();
+            var elapsedTime = stopwatch.ElapsedMilliseconds;
+            Debug.WriteLine($"{elapsedTime}");
+        }
+    }
+}
