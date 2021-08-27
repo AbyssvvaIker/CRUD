55541035301d1fbc7d331834fc151eb55221e1b4
Krzysztof Koleczko <krzysztof.koleczko@zonifero.com>
2021-08-27 16:54:58 +0200

    fix: remodel stopWath filter usage of logger

--- Warehouse.Common/Filters/StopwatchFilter.cs
+++ Warehouse.Common/Filters/StopwatchFilter.cs
@@ -1,21 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
-using System;
-using System.Collections.Generic;
+using Serilog;
 using System.Diagnostics;
-using System.Text;
-using Serilog;
 using System.Threading.Tasks;
-using Serilog;
 
 namespace Warehouse.Common.Filters
 {
     public class StopwatchFilter : IAsyncActionFilter
     {
-        protected readonly ILogger _logger;
-        public StopwatchFilter(ILogger logger)
-        {
-            _logger = logger;
-        }
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
@@ -23,8 +14,7 @@ namespace Warehouse.Common.Filters
             stopwatch.Stop();
             var elapsedTime = stopwatch.ElapsedMilliseconds;
             var log = $"Action: {context.ActionDescriptor.DisplayName} \n\t ExecutionTime: {elapsedTime} ms";
-            Debug.WriteLine(log);
-            _logger.Debug(log);
+            Log.Error(log);
         }
     }
 }
